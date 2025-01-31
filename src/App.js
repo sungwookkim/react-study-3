c710f6ceda04bc913e5be20d16627e5f3465c3a6
sungwookkim <rnsqka2000@gmail.com>
2025-01-31 10:55:53 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Profile from "./Profile.js";
+import Gallery from "./Gallery.js";
+
+export default function App() {
+  return (
+    <div>
+      <Profile />
+      <Gallery />
+    </div>
+  );
+}
